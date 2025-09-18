e765c355a6ed25c319f033cae2f0eabdca50735c
Seka100 <s_sapakov@kbtu.kz>
2025-09-18 11:12:46 +0600

    Add files via upload

---
+++ laba1.py
@@ -0,0 +1,93 @@
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!")
+
+
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+
+
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+
+
+
+x = "awesome"
+
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+
+print("Python is " + x)
+
+
+
+
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+
+
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+
+
+
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+
+
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
